a5bd6be272420dfff0c6330f608314600cf48b11
curious-neeraj <neerajjain.dev@gmail.com>
2024-01-26 23:48:27 +0530

    Basic set to call weather API

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// use godotenv package to load .env file and return value of key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
 
 func main() {
 	fmt.Println("So we're building a basic weather info cli tool using Golang. Let's GO!!")
+
+	// load apiKey value and set location
+	apiKey := goDotEnvVariable("API_KEY")
+	location := "Raipur"
+
+	// try to call the api (with apikey) and get response data
+	body, _ := http.Get("https://api.weatherapi.com/v1/forecast.json?q=" + location + "&days=1&key=" + apiKey)
+	fmt.Println(body.Status)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/curious-neeraj/weather_info_cli
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1 // indirect
